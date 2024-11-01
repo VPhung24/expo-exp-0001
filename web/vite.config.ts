499f647298df36613c8c719c199900359141790f
Vivian Phung <dev+github@vivianphung.com>
2024-10-31 17:40:44 -0700

    output dir

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist', // Output to root directory
+    emptyOutDir: true,
+  },
   server: {
     middlewareMode: false,
     headers: {
